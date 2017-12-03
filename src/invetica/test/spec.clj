df134e9dbcb7babbf6ec87844244c6c857db3035
James Conroy-Finn <git@invetica.co.uk>
2017-12-03 17:58:07 +0000

    Add check-asserts

--- src/invetica/test/spec.clj
+++ src/invetica/test/spec.clj
@@ -7,7 +7,26 @@
    ;;
    ;; See https://github.com/clojure-emacs/cider/issues/1841.
    [clojure.test.check]
-   [clojure.test :refer [is]]))
+   [clojure.test :as t :refer [is]]
+   [clojure.spec.alpha :as s]))
+
+;; -----------------------------------------------------------------------------
+;; Fixtures
+
+(defn check-asserts
+  "Convenience function compatible with `clojure.test/use-fixtures`, that will
+  enable checking of clojure.spec assertions.
+
+  E.g.
+
+      (clojure.test/use-fixtures :once test.spec/check-asserts)"
+  [f]
+  (let [before (s/check-asserts?)]
+    (try
+      (s/check-asserts true)
+      (f)
+      (finally
+        (s/check-asserts before)))))
 
 (defn instrument
   "Convenience function compatible with `clojure.test/use-fixtures`, that will
@@ -22,6 +41,11 @@
     (f)
     (finally
       (stest/unstrument))))
+
+(def once-fixtures
+  "Spec-related fixtures compatible with `clojure.test/use-fixtures`. Enables
+  checking of assertions and instrumentation of functions."
+  (t/join-fixtures [check-asserts instrument]))
 
 (defn is-well-specified
   "Checks all of the vars in namespace `ns`, and pretty prints any failures."
